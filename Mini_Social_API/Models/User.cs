73bd251af76d746d36f683e4c62af76d4b6055d9
tien_lc <lycongtien1312004@gmail.com>
2026-05-15 18:53:58 +0700

    initial commit

---
+++ Mini_Social_API/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace Mini_Social_API.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string PasswordHash { get; set; } = null!;
+        public string? AvatarUrl { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
