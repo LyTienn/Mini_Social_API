15523dc258256ca476f8c93fee6d872e2a136f6c
tien_lc <lycongtien1312004@gmail.com>
2026-05-19 03:09:28 +0700

    test migrations

--- Mini_Social_API/Program.cs
+++ Mini_Social_API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using Mini_Social_API.Data;
 
 namespace Mini_Social_API
 {
@@ -12,7 +14,9 @@ namespace Mini_Social_API
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
-
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(
+                    builder.Configuration.GetConnectionString("DefaultConnection")));
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
